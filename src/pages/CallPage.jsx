92b01adf0840686c8e28a239ad91c43fafe341c9
usernamedinesh <borod9200@gmail.com>
2025-03-21 01:51:35 +0530

    remove src

--- src/pages/CallPage.jsx
+++
@@ -1,9 +0,0 @@
-const CallPage = () => {
-  return (
-    <h1 className="m-7 text-3xl font-black leading-[48px] text-black dark:text-white capitalize">
-      Comming soon...
-    </h1>
-  );
-};
-
-export default CallPage;
--- src/section/chat/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ChatList } from "./ChatList";
-export { default as MessageInbox } from "./Inbox";
-export { default as AiInbox } from "./AiInbox";
